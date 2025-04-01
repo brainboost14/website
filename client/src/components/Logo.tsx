d1fb9cba16565c06ca32979442f854bb11089164
brainboost14 <brainboostacad@gmail.com>
2025-04-01 23:48:32 +0530

    v3-try2

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import LogoImage from "logo.png"; // Ensure this path is correct
+// import LogoImage from "logo.png"; // Ensure this path is correct
 
 interface LogoProps {
   size?: number;
 }
 
 export const Logo: React.FC<LogoProps> = ({ size = 60 }) => {
-  return <img src={LogoImage} alt="Logo" style={{ width: size, height: size }} />;
+  return <img src="./logo.png" alt="Logo" style={{ width: size, height: size }} />;
 };
